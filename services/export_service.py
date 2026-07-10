a7e6cc88df1d985e5d6f9e287cbb67cc6a129473
ViiTooRMD <106347686+ViiTooRMD@users.noreply.github.com>
2026-07-10 14:18:34 -0300

    Update export_service.py

--- services/export_service.py
+++ services/export_service.py
@@ -1,1 +1,32 @@
+from io import BytesIO
 
+import pandas as pd
+
+
+class ExportService:
+    @staticmethod
+    def to_excel(
+        result_dataframe: pd.DataFrame,
+        summary_dataframe: pd.DataFrame,
+    ) -> bytes:
+        output = BytesIO()
+
+        with pd.ExcelWriter(
+            output,
+            engine="openpyxl",
+        ) as writer:
+            result_dataframe.to_excel(
+                writer,
+                sheet_name="Resultado_Detalhado",
+                index=False,
+            )
+
+            summary_dataframe.to_excel(
+                writer,
+                sheet_name="Resumo",
+                index=False,
+            )
+
+        output.seek(0)
+
+        return output.getvalue()
